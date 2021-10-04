a52cc74ac53d0105a03e7dbd84d884d7b8c6733c
AirFast <airfast.88@gmail.com>
2021-10-04 14:44:34 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
+import { Switch, Route, NavLink } from 'react-router-dom';
 import logo from './logo.svg';
 import styles from './App.module.css';
 
-function App() {
+// Pages
+import home from './pages/home';
+import wishlist from './pages/wishlist';
+import cart from './pages/cart';
+
+const App = () => {
   return (
-    <div className={styles.app}>
-      <header className={styles.app_header}>
-        <div className={styles.logo_block}>
-          <img src={logo} className={styles.app_logo} alt="logo" />
-          <span className={styles.site_name}>React shop</span>
-        </div>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <header className={styles.app_header}>
+          <NavLink to={'/'}>
+            <div className={styles.logo_block}>
+              <img src={logo} className={styles.app_logo} alt="logo" />
+              <span className={styles.site_name}>React shop</span>
+            </div>
+          </NavLink>
+          <ul>
+            <li>
+              <NavLink to={'/wishlist'}>Wishlist</NavLink>
+            </li>
+            <li>
+              <NavLink to={'/cart'}>Cart</NavLink>
+            </li>
+          </ul>
+        </header>
+      </div>
+      <Switch>
+        <Route exact path={'/'} component={home} />
+        <Route exact path={'/wishlist'} component={wishlist} />
+        <Route exact path={'/cart'} component={cart} />
+      </Switch>
+    </>
+
   );
 }
 
