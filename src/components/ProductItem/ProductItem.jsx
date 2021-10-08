053e9ef4f18217543baea4f5da7bd97b55c26d3a
AirFast <airfast.88@gmail.com>
2021-10-08 12:10:13 +0300

    item

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -6,6 +6,7 @@ const ProductItem = ({ product }) => {
         <div className='col-3 mt-20 mb-20'>
             <article className={styles.product_item}>
                 <img src={`./img/${product.image}`} alt="logo" />
+                <h3 className={styles.title}>{product.title}</h3>
             </article>
         </div>
     );
