b7dc8015216522e8bf38d4fda7f0329228912aba
AirFast <airfast.88@gmail.com>
2021-10-12 13:02:39 +0300

    cart

--- src/components/Loader/MiniLoader.jsx
+++ src/components/Loader/MiniLoader.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './Loader.module.css';
 
-const MiniLoader = () => {
+const MiniLoader = ({ size }) => {
     return (
-        <div className={styles.mini_loader}></div>
+        <div className={styles.mini_loader} style={{width: size, height: size}}></div>
     );
 };
 
--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -19,14 +19,16 @@ const ProductItem = ({ product }) => {
     return (
         <div className='col-3 mt-20 mb-20'>
             <article className={styles.product_item}>
-                <button className={styles.add_to_wishlist} title='Add to Wishlist'><Heart size={20} /></button>
+            {product.isPending
+                        ? <div className={styles.mini_loader}><MiniLoader size={18} /></div>
+                        : <button className={styles.add_to_wishlist} title='Add to Wishlist'><Heart size={20} /></button>}
                 {/* <button className={styles.remove_from_wishlist} title='Remove from Wishlist'><HeartFill size={20} /></button> */}
                 <img src={`./img/${product.image}`} alt="logo" />
                 <h3 className={styles.title}>{product.title}</h3>
                 <div className={styles.product_item_footer}>
                     <span className={styles.price}>${product.price}</span>
                     {product.isPending
-                        ? <MiniLoader />
+                        ? <MiniLoader size={32} />
                         : !product.isAddedToCart
                             ? <button className={styles.add_to_cart} title='Add to Cart' onClick={handleAddToCart}><Plus size={28} /></button>
                             : <button className={styles.remove_from_cart} title='Remove from Cart' onClick={handleRemoveFromCart}><Check size={28} /></button>}
